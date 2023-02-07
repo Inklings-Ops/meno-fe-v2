8b24575359abdb37b555143690a04cbda996058d
David Michael II <knowdavidmichael@gmail.com>
2023-02-07 02:35:25 +0100

    config: remove unused packages

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,22 +5,16 @@
 import FlutterMacOS
 import Foundation
 
-import agora_rtc_engine
 import connectivity_plus
 import firebase_core
-import firebase_messaging
 import flutter_secure_storage_macos
-import iris_event
 import rive_common
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AgoraRtcNgPlugin.register(with: registry.registrar(forPlugin: "AgoraRtcNgPlugin"))
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  IrisEventPlugin.register(with: registry.registrar(forPlugin: "IrisEventPlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,10 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  agora_rtc_engine
   connectivity_plus
   flutter_secure_storage_windows
-  iris_event
   permission_handler_windows
   rive_common
 )
