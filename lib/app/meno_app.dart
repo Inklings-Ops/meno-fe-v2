dc2367e599e2eaa30a222010cd2a4c4d74a6327d
David Michael II <knowdavidmichael@gmail.com>
2023-02-14 21:23:28 +0100

    fix:  - Terms of use  - Privacy Policy  - Text scaling

--- lib/app/meno_app.dart
+++ lib/app/meno_app.dart
@@ -36,10 +36,6 @@ class MenoApp extends StatelessWidget {
             onGenerateTitle: (context) => 'Meno',
             routerDelegate: _router.delegate(),
             routeInformationParser: _router.defaultRouteParser(),
-            builder: (context, widget) => MediaQuery(
-              data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
-              child: widget!,
-            ),
           ),
         );
       },
